7aedb84741ea4b877e0d6af9bdb693aaa6d8cfcc
Keir Whitlock <keir@whitlock.it>
2024-11-26 23:07:28 +0000

    2.7

--- main.go
+++ main.go
@@ -5,14 +5,31 @@ import (
 	"net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+
+	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:mail@example.com\">mail@exmaple.com</a>")
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 
 	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
 }
 
+func pathHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	default:
+		http.Error(w, "Page not found", http.StatusNotFound)
+	}
+}
+
 func main() {
-	http.HandleFunc("/", handlerFunc)
+	http.HandleFunc("/", pathHandler)
 
 	fmt.Println("Starting the server on :3000...")
 	http.ListenAndServe(":3000", nil)
