abff27933db29378cee66db8b5c67d5b27382dd3
Keir Whitlock <keir@whitlock.it>
2024-11-27 22:54:49 +0000

    2.9

--- main.go
+++ main.go
@@ -18,6 +18,17 @@ func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func pathHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	default:
+		http.Error(w, "Page not found", http.StatusNotFound)
+	}
+}
+
 func contactHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 
@@ -31,8 +42,6 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	var router = Router{}
-
 	fmt.Println("Starting the server on :3000...")
-	http.ListenAndServe(":3000", router)
+	http.ListenAndServe(":3000", http.HandlerFunc(pathHandler))
 }
