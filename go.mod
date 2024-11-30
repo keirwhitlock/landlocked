f778bfdd8bf4bbfe7a9bbd5cce834eb3aedb74b4
Keir Whitlock <keir@whitlock.it>
2024-11-30 18:20:51 +0000

    4.4

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/keirwhitlock/lenslocked
 
 go 1.22.3
+
+require github.com/go-chi/chi/v5 v5.1.0 // indirect
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/go-chi/chi/v5"
 	"net/http"
 )
 
@@ -39,8 +40,15 @@ func faqHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	var router Router
+	r := chi.NewRouter()
+
+	r.Get("/", homeHandler)
+	r.Get("/contact", contactHandler)
+	r.Get("/faq", faqHandler)
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "Page not found", http.StatusNotFound)
+	})
 
 	fmt.Println("Starting the server on :3000...")
-	http.ListenAndServe(":3000", router)
+	http.ListenAndServe(":3000", r)
 }
